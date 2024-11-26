4f32af4b6301b2140b3f295bead2df64c6e11e5e
ranodadge <ranodadgekiyo@ddsciencehs.dje365.kr>
2024-11-26 19:25:48 +0900

    FLAG

---
+++ function/Insta/flagcheck.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const FLAG_FILE = 'json/FLAG.json';
+
+function setFlag() {
+    const currentDate = new Date();
+    const flag = {
+        year: currentDate.getFullYear(),
+        month: currentDate.getMonth(),
+        date: currentDate.getDate(),
+    };
+    fs.writeFileSync(FLAG_FILE, JSON.stringify(flag, null, ' '), 'utf8');
+}
+
+function getFlag(){
+    if (fs.existsSync(FLAG_FILE)) {
+        const data = fs.readFileSync(FLAG_FILE, 'utf8');
+        const flag = JSON.parse(data);
+        const currentDate = new Date();
+        return flag.year === currentDate.getFullYear() && flag.month === currentDate.getMonth() && flag.date === currentDate.getDate();
+    }
+    return false;
+}
+
+module.exports = {
+    setFlag,
+    getFlag
+}
