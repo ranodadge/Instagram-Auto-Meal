7af0721483c70c9d5c7745cb78ecfa7a601d33b7
ranodadge <ranodadgekiyo@ddsciencehs.dje365.kr>
2024-11-26 13:46:43 +0900

    webhook sender

--- function/Insta/webhook.js
+++ function/Insta/webhook.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 const { WebhookClient } = require('discord.js');
 
 async function sendwebhook(buffer1, buffer2, buffer3) {
-    const webhookClient = new WebhookClient({ url: process.env.webhook });
-  
-    try {
-      webhookClient.send({
-        content: "🏫 | 성공적으로 게시물을 올렸습니다. <@!890187595517157377>",
-        username: "동신과고 급식로그",
-        files: [buffer1, buffer2, buffer3],
-      });
-      console.log("성공적으로 로그를 보냈습니다.");
-    } catch (error) {
-      console.error("웹훅 전송 중 오류 발생" + error);
-    }
-  }
+  const webhookClient = new WebhookClient({ url: process.env.webhook });
 
-  module.exports = {
-    sendwebhook
+  try {
+    webhookClient.send({
+      content: "🏫 | 성공적으로 게시물을 올렸습니다. <@!890187595517157377>",
+      username: "동신과고 급식전송로그",
+      files: [buffer1, buffer2, buffer3],
+    });
+    console.log("성공적으로 로그를 보냈습니다.");
+  } catch (error) {
+    console.error("웹훅 전송 중 오류 발생" + error);
   }
+}
+
+module.exports = {
+  sendwebhook
+}
