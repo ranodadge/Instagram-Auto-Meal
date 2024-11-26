792e85116f59cbc7c4694cf0a490638329fcdf31
ranodadge <ranodadgekiyo@ddsciencehs.dje365.kr>
2024-11-26 15:46:56 +0900

    webhook log

--- function/Insta/publish.js
+++ function/Insta/publish.js
@@ -1,8 +1,16 @@
 require('dotenv').config();
+const fs = require('fs');
 const { IgApiClient } = require('instagram-private-api');
+const { login } = require('./login');
 const { drawmenu } = require('./writemenu');
 const { sendwebhook } = require('./webhook');
 
 async function publish(choice){
+    try{
+        const instagram = await login();
+        await drawmenu(choice);
+    } catch(e){
+        
+    }
     
 }
--- function/Insta/webhook.js
+++ function/Insta/webhook.js
@@ -1,13 +1,34 @@
 require('dotenv').config();
+const fs = require('fs');
 const { WebhookClient } = require('discord.js');
 
-async function sendwebhook(buffer1, buffer2, buffer3) {
+async function startlog(){
+  const webhookClient = new WebhookClient({ url: process.env.webhook });
+  let file =[];
+  for(let i = 1; i < 4; i++){
+    file.push(fs.readFileSync(`photo/menu/mealImage${i}.jpg`))
+  }
+  try {
+    webhookClient.send({
+      content:
+        "성공적으로 작동을 시작하였습니다. <@!890187595517157377>\n앞으로 보내질 급식 사진 상태입니다.",
+      username: "동신과고 급식 시작로그",
+      files: file,
+    });
+    console.log("성공적으로 로그를 보냈습니다.");
+  } catch (error) {
+    console.error("웹훅 전송 중 오류 발생" + error);
+  }
+}
+
+
+async function successlog(buffer1, buffer2, buffer3) {
   const webhookClient = new WebhookClient({ url: process.env.webhook });
 
   try {
     webhookClient.send({
       content: "🏫 | 성공적으로 게시물을 올렸습니다. <@!890187595517157377>",
-      username: "동신과고 급식전송로그",
+      username: "동신과고 급식 전송로그",
       files: [buffer1, buffer2, buffer3],
     });
     console.log("성공적으로 로그를 보냈습니다.");
@@ -16,6 +37,26 @@ async function sendwebhook(buffer1, buffer2, buffer3) {
   }
 }
 
+async function errorlog(error) {
+  const webhookClient = new WebhookClient({ url: process.env.webhook });
+  const errorembed = new EmbedBuilder()
+    .setTitle("에러 발생")
+    .setDescription(`\`\`\`bash\n${error}\n\`\`\``)
+    .setColor("Red");
+  try {
+    webhookClient.send({
+      content: "<@!890187595517157377>",
+      username: "동신과고 급식 오류로그",
+      embeds: [errorembed],
+    });
+    console.log("성공적으로 에러로그를 보냈습니다.");
+  } catch (error) {
+    console.error("웹훅 전송 중 오류 발생" + error);
+  }
+}
+
 module.exports = {
-  sendwebhook
+  startlog,
+  successlog,
+  errorlog
 }
