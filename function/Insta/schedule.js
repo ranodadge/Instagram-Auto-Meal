770b3b66a7e18a7a89bb23d6eefb7be768cf5838
ranodadge <ranodadgekiyo@ddsciencehs.dje365.kr>
2024-12-13 13:50:27 +0900

    change activity

--- function/Insta/schedule.js
+++ function/Insta/schedule.js
@@ -2,6 +2,8 @@ const cron = require('node-cron')
 const { getFlag } = require('./flagcheck');
 const { publish } = require('./publish')
 const { randomlog, startlog, errorlog } = require('./webhook');
+const { client } = require('../../index');
+const { ActivityType } = require('discord.js');
 
 let cronJob = cron.ScheduledTask || null;
 
@@ -13,6 +15,8 @@ function scheduleRandomJob() {
     const minute = Math.floor(Math.random() * 60);
     const hour = Math.floor(Math.random() * 1) + 20;
     const cronExpression = `${minute} ${hour} * * 0-4`;
+
+    client.user.setActivity({ name: `오늘 전송 시간 오후${hour}시 ${minute}분`, type: ActivityType.Playing });
 
     cronJob = cron.schedule(cronExpression, () => {
         if(getFlag()) return errorlog("이미 오늘 급식을 보냈습니다.");
@@ -29,6 +33,8 @@ function firstschedule() {
     const minute = Math.floor(Math.random() * 60);
     const hour = Math.floor(Math.random() * 1) + 20;
     const cronExpression = `${minute} ${hour} * * 0-4`;
+
+    //client.user.setActivity({ name: `오늘 전송 시간 오후${hour}시 ${minute}분`, type: ActivityType.Playing });
 
     startlog(hour, minute);
 
