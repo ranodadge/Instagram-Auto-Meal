c83275e06bfc42041d2cc18acb3c1802f02c5fa1
ranodadge <ranodadgekiyo@ddsciencehs.dje365.kr>
2024-12-13 13:48:45 +0900

    startlog one time

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -9,6 +9,5 @@ module.exports = {
     async execute(client) {
         console.log(`${client.user.username}(으)로 로그인되었습니다.`);
         client.user.setActivity({ name: "인스타 확인", type: ActivityType.Playing });
-        startlog();
     }
 }
